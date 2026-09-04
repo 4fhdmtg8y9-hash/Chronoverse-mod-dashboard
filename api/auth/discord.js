032caae6ee97b0100ddce7242c02deed51258037
4fhdmtg8y9-hash <4fhdmtg8y9@privaterelay.appleid.com>
2026-09-04 14:44:16 +0200

    Create discord.js

---
+++ api/auth/discord.js
@@ -0,0 +1,15 @@
+export default function handler(req, res) {
+  const clientId = process.env.DISCORD_CLIENT_ID;
+
+  const redirectUri =
+    "https://chronoverse-mod-dashboard.vercel.app/api/auth/callback";
+
+  const discordUrl =
+    "https://discord.com/oauth2/authorize" +
+    "?client_id=" + encodeURIComponent(clientId) +
+    "&response_type=code" +
+    "&redirect_uri=" + encodeURIComponent(redirectUri) +
+    "&scope=identify";
+
+  res.redirect(302, discordUrl);
+}
