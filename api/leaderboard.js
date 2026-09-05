6e88e79ddec718a53cd7bb528011eed7748aeb16
4fhdmtg8y9-hash <4fhdmtg8y9@privaterelay.appleid.com>
2026-09-05 02:08:28 +0200

    Update leaderboard.js

--- api/leaderboard.js
+++ api/leaderboard.js
@@ -1,6 +1,3 @@
 export default function handler(req, res) {
-  return res.status(200).json({
-    success: true,
-    message: "Leaderboard API is working!",
-  });
+  res.status(200).send("LEADERBOARD WORKS");
 }
